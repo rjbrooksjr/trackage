0973ea606e683a4860910ab23b916594ced5fb27
rich <rbrooks@gmail.com>
2020-08-10 21:55:38 -0400

    help form work

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -21,6 +22,7 @@ import { HelpComponent } from './help/help.component';
     BrowserModule,
     AppRoutingModule,
     ReactiveFormsModule,
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/help/help.component.ts
+++ src/app/help/help.component.ts
@@ -1,15 +1,40 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, Validators, FormGroup, AbstractControl } from '@angular/forms';
+import { HttpService } from '../http.service';
+import { HelpForm } from '../common/types';
 
 @Component({
-  selector: 'app-help',
+  selector: 'help',
   templateUrl: './help.component.html',
   styleUrls: ['./help.component.scss']
 })
 export class HelpComponent implements OnInit {
+  helpForm: FormGroup;
+  formSubmitted = false;
 
-  constructor() { }
+  constructor(private formBuilder: FormBuilder, private httpService: HttpService) { }
 
   ngOnInit(): void {
+    this.helpForm = this.formBuilder.group({
+      email: new FormControl('', [
+        Validators.required,
+        Validators.email,
+      ]),
+      text: new FormControl('', [
+        Validators.required,
+      ]),
+    });
   }
 
+  get email(): AbstractControl {
+    return this.helpForm.get('email');
+  }
+
+  get text(): AbstractControl {
+    return this.helpForm.get('text');
+  }
+
+  sendHelp(data: HelpForm): void {
+    this.httpService.sendHelp(data).subscribe(() => this.formSubmitted = true);
+  }
 }
